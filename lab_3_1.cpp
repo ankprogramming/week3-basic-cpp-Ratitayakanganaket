fab237e75d55e308993026e5ac1687a723552342
Unknown <kanganaket42@gmail.com>
2017-11-27 18:10:06 +0700

    ทำโจทย์ข้อ1

--- lab_3_1.cpp
+++ lab_3_1.cpp
@@ -4,6 +4,13 @@ using namespace std;
 
 int main() {
   // เขียนโปรแกรมเพื่อแสดงผลตามที่กำหนด
-  cout << "Mr. Anukul" << endl;
+  cout <<"Mr."<<endl;
+  cout << "Mr. Ratitaya " << endl;
+  cout << "last names kanganaket" << endl;
+  cout << "nickname Chompoo" << endl;
+  cout << "sex Female" << endl;
+  cout << "Birthday 19 may 1999" << endl;
+  cout << "Hobby is play food ball" << endl;
+  cout << "Status Single" << endl;
   return 0;
 }
